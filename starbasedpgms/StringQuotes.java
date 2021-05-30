f317cc7a40164dd10bb2127f03a080e7fbb52280
raghuveermh30 <46840829+raghuveermh30@users.noreply.github.com>
2021-05-30 19:02:21 +0530

    Add files via upload

---
+++ starbasedpgms/StringQuotes.java
@@ -0,0 +1,14 @@
+package starbasedpgms;
+
+public class StringQuotes {
+
+    public static void main(String[] args) {
+
+        System.out.println("/\"Hello\"/");
+
+        System.out.println("/'Hello'/");
+
+        System.out.println("I love \"java\" and \"programming\" and \"movies\"");
+
+    }
+}
